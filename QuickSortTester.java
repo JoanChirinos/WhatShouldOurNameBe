696f1685789ae3063e81abdfea40780879e17150
JoanChirinos <jchirinos@stuy.edu>
2018-03-13 20:52:46 -0400

    Added stuff

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -32,10 +32,10 @@ public class QuickSortTester {
     ArrayList<Double[]> timeStore = new ArrayList<Double[]>();
 
     //trying different sizes of array
-    for (int size = 1000; size <= 20000; size += 100) {
+    for (int size = 1000; size <= 40000; size += 100) {
       //trying each size a bunch of times
       for (int test = 0; test < 100; test++) {
-        Double[] testArr = {size * 1.0, time(size)}; //Things we learned: you can put function calls into an array!
+        Double[] testArr = {size * 1.0, time(size)}; //Things we learned: you can put method calls into an array!
         timeStore.add(testArr);
       }
     }
