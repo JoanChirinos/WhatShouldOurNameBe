ac93bd905a61777c0582d3f2ec0adc18570be5f7
JoanChirinos <jchirinos@stuy.edu>
2018-03-14 14:12:11 -0400

    Reverting back to old data things

--- QuickSortTester.java
+++ QuickSortTester.java
@@ -40,7 +40,7 @@ public class QuickSortTester {
     ArrayList<Double[]> timeStore = new ArrayList<Double[]>();
 
     //trying different sizes of array
-    for (int size = 1000; size <= 160000; size += 5000) {
+    for (int size = 1000; size <= 40000; size += 100) {
       //trying each size a bunch of times
       for (int test = 0; test < 100; test++) {
         Double[] testArr = {size * 1.0, time(size)}; //Things we learned: you can put method calls into an array!
